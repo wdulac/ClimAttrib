0d6a82cf389e8990d85d17c93c01a39de17d6f15
DULAC William <william.dulac@meteo.fr>
2025-01-21 17:59:11 +0100

    Progress on header

--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,37 @@
 from dash import html, callback, Input, Output
 import dash_mantine_components as dmc
 
-header = html.H1("Mon application avec carte interactive", style={'textAlign': 'center'})
+
+debug_style = {
+    "border": f"1px solid {dmc.DEFAULT_THEME['colors']['indigo'][4]}",
+    "textAlign": "center"
+}
+
+header = html.Div(
+    children=[
+        dmc.Grid(
+            children=[
+                dmc.GridCol(
+                    children=[
+                        html.H1("Extreme Event Tracker", id='page-title')
+                    ], style=debug_style, span=6
+                ),
+                dmc.GridCol(
+                    children=[
+                        dmc.Group(
+                            children=[
+                                dmc.Text("1"),
+                                dmc.Text("2"),
+                                dmc.Text("3")
+                            ], id="header-right"
+                        )
+                    ], style=debug_style, span=6
+                )
+            ]
+        )
+    ],
+    # children=[
+    #     html.H1("Extreme Event Tracker", id='page-title')
+    # ],
+    className='header-parent-div'
+)
