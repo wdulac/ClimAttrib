4ec05cfbeb43df3264c4c9a176d8c81eaf0c2c3f
DULAC William <william.dulac@meteo.fr>
2025-01-24 09:46:42 +0100

    Added demo anchors to the footer

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,4 +1,22 @@
 from dash import html
 import dash_mantine_components as dmc
 
-footer = html.Footer("", className='footer-parent')
+footer = html.Footer(
+    children=[
+        dmc.Group(
+            children=[
+                html.A(
+                    href="",
+                    children="Nous contacter",
+                    className="footer-bottom-link"
+                ),
+                html.A(
+                    href="",
+                    children="Politique de confidentialité",
+                    className="footer-bottom-link"
+                )
+            ], className="footer-row-container"
+        ),
+    ],
+    className='footer-parent'
+)
