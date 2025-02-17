7d39eb7634480009c746b4eb4e3ec50f38dcda7a
DULAC William <william.dulac@meteo.fr>
2025-02-17 11:32:42 +0100

    Refactor header code

--- src/components/header.py
+++ src/components/header.py
@@ -2,10 +2,71 @@ from dash import html
 import dash_mantine_components as dmc
 
 
-debug_style = {
-    "border": f"1px solid {dmc.DEFAULT_THEME['colors']['indigo'][4]}",
-    "textAlign": "center"
-}
+# debug_style = {
+    # "border": f"1px solid {dmc.DEFAULT_THEME['colors']['indigo'][4]}",
+    # "textAlign": "center"
+# }
+
+### Individual header items/buttons
+
+_about_button = html.A(
+    href="",
+    children=[
+        dmc.Button("À propos", variant='subtle')
+    ]
+)
+
+_language_menu = dmc.Menu(
+    children=[
+        dmc.MenuTarget(dmc.Button("EN / FR", variant='subtle')),
+        dmc.MenuDropdown(
+            children=[
+                dmc.MenuItem(
+                    "English",
+                    href=""
+                ),
+                dmc.MenuItem(
+                    "Français",
+                    href=""
+                )
+            ]
+        )
+    ],
+    trigger='hover',
+    openDelay=100,
+    closeDelay=400,
+    id='language-menu'
+)
+
+_github_action_button = html.A(
+    href='https://github.com/wdulac/EET-app',
+    target='_blank',
+    className='github-logo',
+    children=[
+        dmc.ActionIcon(
+            variant='subtle',
+            children=[
+                dmc.Image(src='/assets/logos/github.svg', w=30)
+            ],
+            size="lg"
+        )
+    ]
+)
+
+### Grouping buttons in a single item
+
+_header_right = dmc.Group(
+    children=dmc.Group(
+        children=[
+            _about_button,
+            _language_menu,
+            dmc.Space(w='10px'), # For even spacing of the buttons
+            _github_action_button
+        ], id='header-buttons'
+    ), id='header-right'
+)
+
+### Composing header with a two-column grid layout
 
 header = html.Header(
     children=[
@@ -14,65 +75,16 @@ header = html.Header(
                 dmc.GridCol(
                     children=[
                         html.H1("Nom de l'application", id='page-title')
-                    ], style=None, span=6
+                    ],
+                    style=None,
+                    span=6
                 ),
                 dmc.GridCol(
-                    children=[
-                        dmc.Group(
-                            children=[
-                                dmc.Group(
-                                    children=[
-                                        html.A(
-                                            href="",
-                                            children=[
-                                                dmc.Button("À propos", variant='subtle')
-                                            ]
-                                        ),
-                                        # dmc.Space(w='xs'),
-                                        dmc.Menu(
-                                            children=[
-                                                dmc.MenuTarget(dmc.Button("EN / FR", variant='subtle')),
-                                                dmc.MenuDropdown(
-                                                    children=[
-                                                        dmc.MenuItem(
-                                                            "English",
-                                                            href=""
-                                                        ),
-                                                        dmc.MenuItem(
-                                                            "Français",
-                                                            href=""
-                                                        )
-                                                    ]
-                                                )
-                                            ],
-                                            trigger='hover',
-                                            openDelay=100,
-                                            closeDelay=400,
-                                            id='language-menu'
-                                        ),
-                                        dmc.Space(w='10px'),
-                                        html.A(
-                                            href='https://github.com/wdulac/EET-app',
-                                            target='_blank',
-                                            className='github-logo',
-                                            children=[
-                                                dmc.ActionIcon(
-                                                    variant='subtle',
-                                                    children=[
-                                                        dmc.Image(src='/assets/logos/github.svg', w=30)
-                                                    ],
-                                                    size="lg"
-                                                )
-                                            ]
-                                        )
-                                    ], id='header-buttons'
-                                )
-                            ], id="header-right"
-                        )
-                    ], style=None, span=6
+                    children=_header_right,
+                    style=None,
+                    span=6
                 )
             ]
         )
-    ],
-    className='header-parent'
+    ], className='header-parent'
 )
