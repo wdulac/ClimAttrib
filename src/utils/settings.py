2c149ae97641ec8acdda69deb4e059979f107216
DULAC William <william.dulac@meteo.fr>
2025-03-18 09:55:58 +0100

    Switch to using an .env file with dotenv

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,4 +1,11 @@
-APP_HOST='127.0.0.1'
-APP_PORT=8080
-APP_DEBUG=False
-APP_SHOW_DASH_DEV_TOOLS=False
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+APP_HOST=os.getenv('HOST')
+APP_PORT=int(os.getenv('PORT'))
+APP_DEBUG=os.getenv('DEBUG').upper() == 'TRUE'
+APP_SHOW_DASH_DEV_TOOLS=os.getenv('SHOW_DASH_DEV_TOOLS').upper() == 'TRUE'
