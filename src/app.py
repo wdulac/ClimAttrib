f90d439a67c73ec1c1da4ef998b9b500f4702891
DULAC William <william.dulac@meteo.fr>
2025-03-31 11:25:54 +0200

    Serve mantine css locally (unpkg unavailable)

--- src/app.py
+++ src/app.py
@@ -16,8 +16,8 @@ server = Flask(__name__)
 application = Dash(
     server=server,
     external_stylesheets=[
-        dmc.styles.NOTIFICATIONS,
-        dmc.styles.DATES,
+        # dmc.styles.NOTIFICATIONS,
+        # dmc.styles.DATES,
         dbc.themes.BOOTSTRAP # Needed for row and columns to work as expected in dbc
     ],
     update_title=None,
