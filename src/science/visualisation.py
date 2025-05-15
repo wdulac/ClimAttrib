19b7c6cb59064fe1259ba7af83a9cc80c8cd7d47
DULAC William <william.dulac@meteo.fr>
2025-05-15 14:30:28 +0200

    Switch from matplotlib to plotly

--- src/science/visualisation.py
+++ src/science/visualisation.py
@@ -1,10 +1,7 @@
 import xarray as xr
 import numpy as np
-import matplotlib
-import matplotlib.figure
-import matplotlib.pyplot as plt
 
-matplotlib.use('agg')
+import plotly.graph_objects as go
 
 CONFIDENCE_INTERVAL=0.05
 
@@ -19,56 +16,131 @@ def PRlink(x: float, e:float=3) -> float:
 
 
 # TODO Make interactive plot
-def plot_probability(stats: xr.DataArray) -> tuple[
-	matplotlib.figure.Figure,
-    plt.Axes
-]:
-    """
-    TODO Docstring
-	"""
-	
-    # Compute probability lower/median/upper bounds for plotting  
-    qstats = stats.\
-	    quantile(
-		    [0.5*CONFIDENCE_INTERVAL, 0.5, 1-0.5*CONFIDENCE_INTERVAL],
-		    dim="sample_MCMC"
-	    ).\
-		assign_coords(
-			quantile=['ql', 'be', 'qu']
-        )
+def plot_probability(stats: xr.DataArray):
 
-    mm     = 1. / 25.4
-    ratio  = 16 / 11
-    nrow   = 1
-    ncol   = 1
-    width  = 180*mm
-    height = width / ncol / ratio * nrow
+    # 1. Calcul des quantiles
+    qstats = stats.quantile(
+        [0.5*CONFIDENCE_INTERVAL, 0.5, 1-0.5*CONFIDENCE_INTERVAL],
+        dim="sample_MCMC"
+    ).assign_coords(quantile=["ql", "be", "qu"])
 
-    colors = ["red","blue"]
-    yticks = np.array([0,1e-12,1e-6,1e-3,1e-2,1/30,1/10,0.2,0.5,1] )
-    yticklabelsL = ["0",r"$10^{-12}$",r"$10^{-6}$",r"$10^{-3}$",r"$10^{-2}$","1/30","1/10","1/5","1/2","1"]
-    yticklabelsR = [r"$\infty$","","","1000","100","30","10","5","2","1"]
+    # 2. Définition des tailles
+    mm = 1. / 25.4
+    ratio = 16 / 11
+    width = 180 * mm * 128  # 96 dpi
+    height = width / ratio
 
+    colors = ['rgba(255,0,0,0.5)', 'rgba(0,0,255,0.5)']
+    yticks = np.array([0,1e-12,1e-6,1e-3,1e-2,1/30,1/10,0.2,0.5,1])
+    yticklabelsL = ["0", "10⁻¹²", "10⁻⁶", "10⁻³", "10⁻²", "1/30", "1/10", "1/5", "1/2", "1"]
+    yticklabelsR = ["∞", "", "", "1000", "100", "30", "10", "5", "2", "1"]
 
-    fig = plt.figure( figsize = (width,height) )
-    ax  = fig.add_subplot( nrow , ncol , 1 )
-    for iqp,qp in enumerate([qstats.loc[:,:,"pF"],qstats.loc[:,:,"pC"]]):
-        ax.plot( qp.time , plink(qp.loc["be",:]) , color = colors[iqp] )
-        ax.fill_between( qp.time , plink(qp.loc["ql",:]) , plink(qp.loc["qu",:]) , color = colors[iqp] , alpha = 0.5 )
-    ax.set_yticks(plink(yticks))
-    ax.set_yticklabels(yticklabelsL)
-    ax.set_ylabel("Probability")
-    ax.set_ylim(plink([0,1]))
+    fig = go.Figure()
 
-    axR = fig.add_subplot( nrow , ncol , 1 , sharex = ax , frameon = False )
-    axR.yaxis.tick_right()
-    axR.set_yticks(plink(yticks))
-    axR.set_yticklabels(yticklabelsR)
-    axR.yaxis.set_label_position( "right" )
-    axR.set_ylabel( "Return period" , rotation = 270 )
-    ax.set_ylim(plink([0,1]))
-    # ax.set_title("Return period of a "+str(input.rl_val())+"°C-event at ["+str(round(rl_calc()[2],2))+"°N ; "+str(round(rl_calc()[3],2))+" °E]")
+    names = ['pF', 'pC']
+    trace_names = ['Factual', 'Counter-factual']
+    for i, (name, tn) in enumerate(zip(names, trace_names)):
+        qp = qstats.loc[:, :, name]
+        time = qp.time.values
+        be = qp.sel(quantile='be').values
+        ql = qp.sel(quantile='ql').values
+        qu = qp.sel(quantile='qu').values
 
-    plt.tight_layout()
+        # Bande inférieure
+        fig.add_trace(go.Scatter(
+            x=time,
+            y=plink(ql),
+            mode='lines',
+            line=dict(color=colors[i], width=0),
+            showlegend=False,
+            hoverinfo='skip',
+            fill=None
+        ))
 
-    return fig, ax
+        # Bande supérieure
+        fig.add_trace(go.Scatter(
+            x=time,
+            y=plink(qu),
+            mode='lines',
+            line=dict(color=colors[i], width=0),
+            fill='tonexty',
+            fillcolor=colors[i],
+            # opacity=0.3,
+            showlegend=False,
+            hoverinfo='skip'
+        ))
+
+        # Courbe médiane
+        fig.add_trace(go.Scatter(
+            x=time,
+            y=plink(be),
+            customdata=np.stack([
+                be,
+                ql,
+                qu,
+                1/be,
+                1/qu,
+                1/ql
+            ], axis=-1),
+            mode='lines',
+            line=dict(color=colors[i], width=2),
+            name=tn,
+            hovertemplate = (
+                "<b>Year</b> : %{x}<br>" +
+                "<b>Probability</b> : %{customdata[0]:.2f}<br>" +
+                "<b>Confidence</b> : From %{customdata[1]:.2f} to %{customdata[2]:.2f}<br>" +
+                "<b>Return period</b> : %{customdata[3]:.1f} years<br>" +
+                "<b>Confidence</b> : From %{customdata[4]:.1f} to %{customdata[5]:.1f} years" +
+                "<extra></extra>"
+            )
+        ))
+
+    # Adding bare minimum trace to get a secondary axis
+    fig.add_trace(go.Scatter(
+        x=[min(time), max(time)],
+        y=[min(be), max(be)],
+        name="yaxis2 data",
+        showlegend=False,
+        line=dict(width=0, color='rgba(0,0,0,0)'),
+        hoverinfo='skip',
+        yaxis="y2"
+    ))
+
+    # 4. Mise en forme de l’axe principal Y
+    fig.update_layout(
+        width=width,
+        height=height,
+        margin=dict(l=60, r=60, t=40, b=40),
+        yaxis=dict(
+            title="Probability",
+            tickvals=plink(yticks),
+            ticktext=yticklabelsL,
+            range=[plink(0), plink(1)],
+            showline=True,
+            linecolor='black',
+            gridcolor='lightgrey',
+        ),
+        yaxis2=dict(
+            title="Return period",
+            overlaying='y',
+            side='right',
+            tickvals=plink(yticks),
+            ticktext=yticklabelsR,
+            showgrid=False,
+            showline=True,
+            range=[plink(0), plink(1)],
+            linecolor='black',
+        ),
+        xaxis=dict(
+            title="Time",
+            range=[1850, 2100],
+            ticks='outside',
+            showline=True,
+            linecolor='black',
+            gridcolor='lightgrey',
+            mirror=True
+        ),
+        plot_bgcolor='white',
+    )
+
+    return fig
