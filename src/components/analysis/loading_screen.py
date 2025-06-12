0f218b05b69e0c2f9ed42d4c5952baeb34d1bdef
DULAC William <william.dulac@meteo.fr>
2025-06-12 15:44:25 +0200

    Improve skeleten (both visually) and in code

--- src/components/analysis/loading_screen.py
+++ src/components/analysis/loading_screen.py
@@ -1,27 +1,24 @@
-from dash.exceptions import PreventUpdate
 import dash_mantine_components as dmc
 
 
-loading_screen = dmc.Box(
+
+def make_skeleton_lines(n, width='100%'):
+    return [
+        dmc.Skeleton(height=10, radius='xl', w=width if i == n - 1 else '100%')
+        for i in range(n)
+    ]
+
+loading_screen = dmc.Box([
     dmc.Grid([
         dmc.GridCol([
             dmc.Stack([
-                dmc.Skeleton(height=50, circle=True, mb='xl'),
-                dmc.Skeleton(height=8, radius='xl'),
-                dmc.Skeleton(height=8, radius='xl'),
-                dmc.Skeleton(height=8, radius='xl', w='70%')
+                dmc.Skeleton(height=75, circle=True, mb='sm'),
+                *make_skeleton_lines(8, width='70%')
             ])
         ], span=6),
 
         dmc.GridCol([
-            dmc.Stack([
-                dmc.Skeleton(height=8, radius='xl'),
-                dmc.Skeleton(height=8, radius='xl'),
-                dmc.Skeleton(height=8, radius='xl'),
-                dmc.Skeleton(height=8, radius='xl'),
-                dmc.Skeleton(height=8, radius='xl', w='70%')
-            ])
-        ], span=6),
-    ], style={'width': '100%'}),
-    className='loading-skeleton'
-)
+            dmc.Stack(make_skeleton_lines(12, width='70%'))
+        ], span=6)
+    ], gutter=100, style={'width': '100%'})
+], className='loading-skeleton')
