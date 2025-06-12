3977908c6a8bc1d89842aefe85a766d9eb32ac89
DULAC William <william.dulac@meteo.fr>
2025-06-12 13:51:30 +0200

    PoC for loading screen prior to carousel

--- src/components/analysis/probability_plot.py
+++ src/components/analysis/probability_plot.py
@@ -6,30 +6,17 @@ from science import attribute_event
 from science import plot_probability
 
 
-# Fancy trick from ChatGPT to behave as if data-types were preserved
-# through JSON conversion after getting stored into a dcc.Store component.
-def _serialize_event(event):
-    """Convertit event en un format sérialisable"""
-    event_copy = event.copy()
-    event_copy["date"] = event_copy["date"].isoformat()  # Convertit datetime en string ISO
-    event_copy["lat_lon"] = [event_copy.pop("lat"), event_copy.pop("lon")]  # Tuple -> Liste
-    return event_copy
-
-
 def _deserialize_event(event):
     """Convertit event stocké en JSON en un dictionnaire avec les bons types"""
     event_copy = event.copy()
     event_copy["date"] = dt.fromisoformat(event_copy["date"])  # Reconversion ISO -> datetime
-    event_copy["lat"], event_copy["lon"] = event_copy.pop("lat_lon")  # Liste -> Tuple
     return event_copy
 
 
 def probability_plot(event: dict):
 
-    serialized_event = _serialize_event(event)
-
     component = html.Div(children=[
-        dcc.Store(id='event-data', data=serialized_event),
+        dcc.Store(id='event-data', data=event),
         dcc.Store(id='plotly-notifier-hook', data=None),
         html.Div(id='plot-container', children='Calcul en cours...')
     ], id='results-parent-container')
--- src/pages/analysis.py
+++ src/pages/analysis.py
@@ -1,14 +1,11 @@
 from dash import register_page, html, dcc, callback, Input, Output
-from dash import clientside_callback, ClientsideFunction
 from dash.exceptions import PreventUpdate
 
-import dash_mantine_components as dmc
-from dash_iconify import DashIconify
-
+import time
 import datetime as dt
 from components import chosen_event
-# from components import event_stats
-from components import probability_plot
+from components import loading_screen
+from components import carousel
 
 import xarray as xr
 import os
@@ -69,94 +66,24 @@ def layout(extreme_type=None,
 
     # Compose and return layout
     layout = html.Div([
-        # html.Div('Bienvenue sur cette page', id='analysis-welcome'),
+        dcc.Store(data=parsed_event, id='event-data'),
         chosen_event(parsed_event), # Dummy component with event's description
-        html.Div([
-            dmc.Carousel([
-                    dmc.CarouselSlide(
-                        dmc.Center(
-                            dmc.Group(
-                                children=[
-                                    probability_plot(parsed_event),
-                                    dmc.Text(
-                                        ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed ipsum ut ",
-                                         "tellus tincidunt ullamcorper at eu metus. Phasellus et mi auctor, molestie "
-                                         "sapien ut, vulputate nisl. Etiam non est vestibulum nibh commodo pulvinar. "
-                                         "Quisque vulputate commodo tellus nec malesuada. Praesent a semper massa. "
-                                         "Aliquam tortor risus, dapibus eget vestibulum vel, rutrum quis lorem. "
-                                         "Vivamus ut cursus nunc. Morbi sit amet rhoncus urna. Quisque volutpat a orci "
-                                         "quis tincidunt. Nullam euismod dictum turpis eget sagittis. Aenean facilisis "
-                                         "est arcu, ac tincidunt ipsum lobortis vitae. Curabitur a enim tristique, "
-                                         "maximus erat sed, pharetra lorem. Proin laoreet congue porttitor. Donec "
-                                         "luctus justo semper ex varius tristique. In ultrices lacus est, a rhoncus "
-                                         "nisi ullamcorper in. Proin placerat tristique convallis."),
-                                        id='lorem-ipsum-demo',
-                                        style={'userSelect': 'text'},
-                                        w=600
-                                    )
-                                ],
-                            h='100%', style={
-                                'justifyContent': 'space-around',
-                                'width': '100%',
-                                'userSelect': 'none'
-                                }
-                            ),
-                        h='100%', bg=dmc.DEFAULT_THEME['colors']['yellow'][2]
-                        )
-                    ),
-                    dmc.CarouselSlide(
-                        dmc.Center(
-                            "Page 2",
-                            bg=dmc.DEFAULT_THEME['colors']['red'][2], c="black", w="100%", h="100%"
-                        )
-                    ),
-                    dmc.CarouselSlide(
-                        dmc.Center(
-                            "Page 3",
-                            bg=dmc.DEFAULT_THEME['colors']['green'][2], c='black', w='100%', h='100%'
-                        )
-                    ),
-                    dmc.CarouselSlide(
-                        dmc.Center(
-                            "Page 4",
-                            bg=dmc.DEFAULT_THEME['colors']['blue'][2], c='black', w='100%', h='100%'
-                        )
-                    )
-                ],
-                orientation='vertical',
-                withIndicators=True,
-                withControls=True,
-                emblaOptions={
-                    'align': 'center',
-                    'loop': False
-                },
-                previousControlIcon=DashIconify(icon="icons8:up-round", width=50),
-                nextControlIcon=DashIconify(icon="icons8:down-round", width=50),
-                classNames={
-                    "indicator": "dmc-indicator",
-                    "control": "dmc-control"
-                },
-                id='my-carousel'
-                )
-            ], className='carousel-container'),
+        html.Div(children=loading_screen, id='content',
+                 className='carousel-container')
         ], className='analysis-container'
     )
     return layout
 
-clientside_callback(
-    ClientsideFunction(
-        namespace='carousel',
-        function_name='toggleBounce'
-    ),
-    Output("my-carousel", "data-toggle-bounce"),
-    Input("my-carousel", "active")
-)
 
-clientside_callback(
-    ClientsideFunction(
-        namespace='carousel',
-        function_name='blockSwiper'
-    ),
-    Output('my-carousel', 'data-lorem-ipsum-swiper'),
-    Input('lorem-ipsum-demo', 'id')
+@callback(
+    Output('content', 'children'),
+    Input('event-data', 'data')
 )
+def test_page(data):
+
+    if not data:
+        raise PreventUpdate
+    
+    else:
+        time.sleep(2)
+        return carousel(data)
