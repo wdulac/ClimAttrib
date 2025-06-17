e6849ca1b1f11a93d1b125c79fa365fd48756dbf
DULAC William <william.dulac@meteo.fr>
2025-06-17 14:08:51 +0200

    Compress responses and use server cache for loaded tiles

--- src/utils/geojson_tiles.py
+++ src/utils/geojson_tiles.py
@@ -1,10 +1,38 @@
 from app import server
 import os
 import json
+
 from flask import request, jsonify
+from flask_compress import Compress
+from functools import lru_cache
+
+server.config['COMPRESS_REGISTER'] = False # Disable default compression
+compress = Compress()
+compress.init_app(server)
+
+TILE_SIZE =10
+GRID_TILES_DIR = "src/assets/static/grid_tiles/"
+
+@lru_cache(maxsize=128)
+def load_tile(x, y):
+    tile_path = os.path.join(GRID_TILES_DIR, f"tile_{x}_{y}.geojson")
+    if os.path.exists(tile_path):
+        with open(tile_path, "r") as f:
+            tile_data = json.load(f)
+            return tile_data["features"]
+    return []
+
+
+def tile_indices_in_bbox(south, west, north, east):
+    x_min = int(west // TILE_SIZE)
+    x_max = int(east // TILE_SIZE)
+    y_min = int(south // TILE_SIZE)
+    y_max = int(north // TILE_SIZE)
+    return [(x, y) for x in range(x_min, x_max + 1) for y in range(y_min, y_max + 1)]
 
 
 @server.route("/grid_tiles")
+@compress.compressed()
 def serve_grid_tiles():
     bounds_str = request.args.get("bounds", None)
     if bounds_str is None:
@@ -15,20 +43,8 @@ def serve_grid_tiles():
     south, west = bounds[0]
     north, east = bounds[1]
 
-    TILE_SIZE = 10
-    def tile_indices_in_bbox(south, west, north, east):
-        x_min = int(west // TILE_SIZE)
-        x_max = int(east // TILE_SIZE)
-        y_min = int(south // TILE_SIZE)
-        y_max = int(north // TILE_SIZE)
-        return [(x, y) for x in range(x_min, x_max + 1) for y in range(y_min, y_max + 1)]
-
     features = []
     for x, y in tile_indices_in_bbox(south, west, north, east):
-        tile_path = f"src/assets/static/grid_tiles/tile_{x}_{y}.geojson"
-        if os.path.exists(tile_path):
-            with open(tile_path, "r") as f:
-                tile_data = json.load(f)
-                features.extend(tile_data["features"])
+        features.extend(load_tile(x, y))
 
     return jsonify({"type": "FeatureCollection", "features": features})
