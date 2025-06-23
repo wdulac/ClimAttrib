b97cd4f312307a0fe7c3993e8a90468d5c2a709a
DULAC William <william.dulac@meteo.fr>
2025-06-23 17:23:19 +0200

    Add PR/FAR plot to carousel

--- src/components/analysis/carousel.py
+++ src/components/analysis/carousel.py
@@ -41,23 +41,11 @@ def carousel(stats):
                     ),
                     dmc.CarouselSlide(
                         dmc.Center(
-                            # PR_FAR_plot(stats),
-                            # 'Page 2',
-                            dmc.Text(
-                                ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed ipsum ut ",
-                                 "tellus tincidunt ullamcorper at eu metus. Phasellus et mi auctor, molestie "
-                                 "sapien ut, vulputate nisl. Etiam non est vestibulum nibh commodo pulvinar. "
-                                 "Quisque vulputate commodo tellus nec malesuada. Praesent a semper massa. "
-                                 "Aliquam tortor risus, dapibus eget vestibulum vel, rutrum quis lorem. "
-                                 "Vivamus ut cursus nunc. Morbi sit amet rhoncus urna. Quisque volutpat a orci "
-                                 "quis tincidunt. Nullam euismod dictum turpis eget sagittis. Aenean facilisis "
-                                 "est arcu, ac tincidunt ipsum lobortis vitae. Curabitur a enim tristique, "
-                                 "maximus erat sed, pharetra lorem. Proin laoreet congue porttitor. Donec "
-                                 "luctus justo semper ex varius tristique. In ultrices lacus est, a rhoncus "
-                                 "nisi ullamcorper in. Proin placerat tristique convallis."),
-                                id='lorem-ipsum-demo',
-                                style={'userSelect': 'text'},
-                                w=600
+                            dmc.Group(
+                                children=[
+                                    PR_FAR_plot(stats)
+                                ],
+                                h='100%'
                             ),
                             bg=dmc.DEFAULT_THEME['colors']['red'][2], c="black", w="100%", h="100%"
                         )
