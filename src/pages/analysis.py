6fbd4d03d5e7779d324a3c2a9d5be997a3cc32b2
DULAC William <william.dulac@meteo.fr>
2025-07-22 11:16:46 +0200

    Rename divs for clarity

--- src/pages/analysis.py
+++ src/pages/analysis.py
@@ -95,9 +95,9 @@ def layout(extreme_type=None,
             disabled=False
         ),
         chosen_event(parsed_event), # Dummy component with event's description
-        html.Div(children=_loading_screen, id='content',
+        html.Div(children=_loading_screen, id='analysis-content',
                  className='carousel-container')
-        ], className='analysis-container'
+        ], className='analysis-container', id='analysis-container'
     )
     return layout
 
@@ -119,7 +119,7 @@ def run_task(data):
     
 # TODO Find better way to retrieve stats datasets later on, than to pass task_id all the way down to create_plotly_figure
 @callback(
-    Output('content', 'children'),
+    Output('analysis-content', 'children'),
     Output('update-interval', 'disabled'),
     Input('update-interval', 'n_intervals'),
     State('task-id', 'data'),
