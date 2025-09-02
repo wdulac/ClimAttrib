137c7e19c831f15b3e60f59f138a0f0f4e4cddcd
DULAC William <william.dulac@meteo.fr>
2025-09-02 09:38:13 +0200

    Type hints and doctstrings

--- src/utils/redis_cache.py
+++ src/utils/redis_cache.py
@@ -1,29 +1,50 @@
 import hashlib, json, pickle, os
 import redis
+import xarray
 
 REDIS_TTL=60*60*48 # 48 hours caching
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
 redis_client = redis.Redis(
-    host=os.getenv('REDIS_HOST', 'localhost'),
-    port=int(os.getenv('REDIS_PORT', 6379)),
+    host=REDIS_HOST,
+    port=REDIS_PORT,
     db=1
 )
 
 
 def make_cache_key(event: dict) -> str:
+    """
+    Create redis key based on an event dict
+    """
+
     payload = json.dumps(event, sort_keys=True, default=str).encode('utf-8')
     return hashlib.sha256(payload).hexdigest()
 
 
-def set_cache(cache_key: str, value, ttl: int = REDIS_TTL):
+def set_cache(cache_key: str, value: xarray.Dataset, ttl: int = REDIS_TTL):
+    """
+    Store attribution dataset to redis with expiration duration
+    """
+
     redis_client.set(cache_key, pickle.dumps(value), ex=ttl)
 
 
-def cache_exists(*args: list):
+def cache_exists(*args: list) -> bool:
+    """
+    Returns true if all keys received as parameters exist in the Redis database
+    """
+
     return redis_client.exists(*args) == len(args)
 
 
-def get_cache(cache_key: str, reset_ttl=REDIS_TTL):
+def get_cache(cache_key: str, reset_ttl=REDIS_TTL) -> xarray.Dataset:
+    """
+    Retrieves stored value associated with a given key and extends the expiration duration.
+
+    Returns None if the key doesn't exist in Redis
+    """
+    
     data = redis_client.get(cache_key)
     if data and reset_ttl:
         redis_client.expire(cache_key, reset_ttl)
