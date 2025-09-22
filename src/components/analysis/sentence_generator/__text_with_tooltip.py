6242ffd29b801a28ee59e738e8b0aa43c4e3cab5
DULAC William <william.dulac@meteo.fr>
2025-09-22 11:14:55 +0200

    Comments and docstring

--- src/components/analysis/sentence_generator/__text_with_tooltip.py
+++ src/components/analysis/sentence_generator/__text_with_tooltip.py
@@ -1,11 +1,17 @@
 import re
 import dash_mantine_components as dmc
 
-
+# Regex for parsing token in the form of [[CI:<label>|<display>]]
 TOKEN_RE = re.compile(r"\[\[CI:(?P<label>[^|\]]+)\|(?P<display>[^\]]+)\]\]")
 
 
 def render_text_with_tooltips(paragraph: str) -> dmc.Text:
+    """
+    Renders a text string into Dash web elements, using dmc.Tooltips to show confidence intervals as tooltips.
+    Tooltips content (i.e label) are extracted from tokens using regex.
+
+    :paragraph: String outputed by jinja2 (template rendering)
+    """
 
     children = [] # Children for the resulting dmc.Text
     pos = 0
@@ -14,9 +20,11 @@ def render_text_with_tooltips(paragraph: str) -> dmc.Text:
             # Text between the start of paragraph and the start of our first token.
             before = paragraph[pos:match.start()] # Text before a CI token
             children.append(before)
-        # Handle the token
+        ## Handle the token
+        # Separate label and value to display from the token
         label = match.group('label')
         display = match.group('display')
+        # Create a tooltip element from them
         children.append(
             dmc.Tooltip(
                 label=label,
@@ -24,7 +32,7 @@ def render_text_with_tooltips(paragraph: str) -> dmc.Text:
                 position="top",
                 children=dmc.Text(display, span=True, fw=700),
                 boxWrapperProps={
-                    "className": "ci", # class defined assets/components/confidence_tooltips.css
+                    "className": "ci", # Styling in assets/components/confidence_tooltips.css
                     "style": {"display": "inline-block"}
                     }
             )
