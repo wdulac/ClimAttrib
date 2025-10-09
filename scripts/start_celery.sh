8dcff42cf0625bb1d7c4f42aecd36a810470fdcb
DULAC William <dulacw@sxamacs>
2025-10-09 17:36:00 +0200

    Disable worker heartbeat

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -5,5 +5,5 @@ APPLICATION="utils.tasks"
 LOGLEVEL="INFO"
 
 # Not sure how to stop gracefully... `pkill -f "celery"` ?
-celery --workdir $WORKDIR -A $APPLICATION worker --loglevel=$LOGLEVEL --concurrency=1 --pool=solo --hostname=worker1@%h &
-celery --workdir $WORKDIR -A $APPLICATION worker --loglevel=$LOGLEVEL --concurrency=1 --pool=solo --hostname=worker2@%h &
+celery --workdir $WORKDIR -A $APPLICATION worker --loglevel=$LOGLEVEL --concurrency=1 --pool=solo --hostname=worker1@%h --without-heartbeat &
+celery --workdir $WORKDIR -A $APPLICATION worker --loglevel=$LOGLEVEL --concurrency=1 --pool=solo --hostname=worker2@%h --without-heartbeat &
