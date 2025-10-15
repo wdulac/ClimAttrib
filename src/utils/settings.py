143b1c630eb7975ca784079d6578cbb15461ea9f
DULAC William <william.dulac@meteo.fr>
2025-10-15 10:58:33 +0200

    Fix parenthesis

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv(ROOT / '.env')
 
 APP_HOST=os.getenv('HOST', '127.0.0.1')
-APP_PORT=int(os.getenv('PORT'), '8000')
+APP_PORT=int(os.getenv('PORT', '8000'))
 APP_DEBUG=os.getenv('DEBUG', 'False').upper() == 'TRUE'
 APP_SHOW_DASH_DEV_TOOLS=os.getenv('SHOW_DASH_DEV_TOOLS', 'False').upper() == 'TRUE'
