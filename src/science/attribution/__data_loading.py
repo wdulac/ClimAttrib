8a66d04edfda4e9558b5cf070f8aea065d028923
DULAC William <william.dulac@meteo.fr>
2026-04-01 11:35:14 +0200

    Streamline Yo and prior file loading in preparation of upcoming calendar files

--- src/science/attribution/__data_loading.py
+++ src/science/attribution/__data_loading.py
@@ -10,26 +10,20 @@ from .__settings import *
 
 def _load_prior(extreme_type: str, computation_method: str, start_date: dt.datetime, stop_date: dt.datetime, duration: int) -> dict:
 
-    if computation_method == 'yearmax':
-        if extreme_type == 'hot':
-            var = f'tmx{duration}d'
-            side = 'right'
-        elif extreme_type == 'cold':
-            var = f'tmn{duration}d'
-            side = 'left'
-    elif computation_method == 'calendar':
-        if duration == 3:
-            if extreme_type == 'hot':
-                window = _best_window_from_range(_datetime_to_doy(start_date),
-                                                _datetime_to_doy(stop_date))
-                var = f'tmx3d15w_{window[0]:03d}-{window[1]:03d}'
-                side = 'right'
-            else:
-                raise NotImplementedError
-        else:
-            raise NotImplementedError
-        
-    clim_file =  DATA / f'prior/{var}_CONSTRAIN_X.nc'
+    if extreme_type == 'hot':
+        var = f"tmx{duration}d"
+        side = 'right'
+    elif extreme_type == 'cold':
+        var = f"tmn{duration}d"
+        side = 'left'
+
+    if computation_method == 'calendar':
+        window = _best_window_from_range(
+            _datetime_to_doy(start_date), _datetime_to_doy(stop_date)
+        )
+        var += f"15w_{window[0]:03d}-{window[1]:03d}"
+
+    clim_file = DATA / 'prior' / computation_method / f"{var}_CONSTRAIN_X.nc"
 
     clim = ank.Climatology.init_from_file(clim_file)
     # Set forcings to CMIP5 (CMIP5 XN file replaced by EBM response to CMIP6 forcings...)
@@ -73,26 +67,23 @@ def _load_obs(lat: float, lon: float, extreme_type: str, computation_method: str
     # Convert to 0 -- 360°
     lon = lon % 360
 
-    if computation_method == 'yearmax':
-        if extreme_type == 'hot':
-            var_name = f'tmx{duration}d'
-            file_suffix = var_name
-        elif extreme_type == 'cold':
-            var_name = f'tmn{duration}d'
-            file_suffix = var_name
-    elif computation_method == 'calendar':
-        if extreme_type == 'hot':
-            var_name = 'tmx3d15w'
-            window = _best_window_from_range(_datetime_to_doy(start_date),
-                                             _datetime_to_doy(stop_date))
-            file_suffix = f"{var_name}_{window[0]:03d}-{window[1]:03d}"
-        elif extreme_type == 'cold':
-            raise NotImplementedError
+    if extreme_type == 'hot':
+        var = f"tmx{duration}d"
+    elif extreme_type == 'cold':
+        var = f"tmn{duration}d"
 
+    file_suffix = var
 
-    Yo_file = DATA / f'Yo/{var_name}/Yo_{file_suffix}.nc'
+    if computation_method == 'calendar':
+        window = _best_window_from_range(
+            _datetime_to_doy(start_date), _datetime_to_doy(stop_date)
+        )
+        var += "15w"
+        file_suffix = f"{var}_{window[0]:03d}-{window[1]:03d}"
 
-    Yo = xr.open_dataset(Yo_file)[var_name].sel(lat=lat, lon=lon)
+    Yo_file = DATA / f'Yo/{var}/Yo_{file_suffix}.nc'
+
+    Yo = xr.open_dataset(Yo_file)[var].sel(lat=lat, lon=lon)
 
     # On remplace l'axe du temps par les années
     Yo = xr.DataArray(Yo.values, dims=Yo.dims,
