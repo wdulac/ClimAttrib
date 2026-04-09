d1397143b0c41ff335b2d2d03655b39e78a5c445
DULAC William <william.dulac@meteo.fr>
2026-04-09 16:23:31 +0200

    Delete wrong imports

--- src/app_platform/web/admin/register.py
+++ src/app_platform/web/admin/register.py
@@ -2,7 +2,7 @@ from app_platform.web.admin import admin_bp
 from app_platform.shared.config import URL_PREFIX
 
 # import pour enregistrer les routes
-from app_platform.web.admin import clear_cache, restart, shell
+from app_platform.web.admin import clear_cache
 
 
 def register_admin_routes(server):
