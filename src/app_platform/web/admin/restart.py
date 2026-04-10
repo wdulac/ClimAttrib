24badf323dcce0338764a50356086e34c7d66ab6
DULAC William <william.dulac@meteo.fr>
2026-04-10 11:14:33 +0200

    Introduce hmac over timestamp to prevent reuse of intercepted HTTP requests

--- src/app_platform/web/admin/restart.py
+++ src/app_platform/web/admin/restart.py
@@ -1,3 +1,4 @@
+import hmac, hashlib, time
 from flask import request, abort, jsonify
 
 from app_platform.web.admin import admin_bp
@@ -7,14 +8,38 @@ import os
 import signal
 import psutil
 
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")
+
+ADMIN_SECRET = os.getenv("ADMIN_SECRET").encode('utf-8')
+HASH = hashlib.sha256
+
+
+def verify_signature(route, timestamp, signature, max_age=30):
+    # Including route into the payload prevents reusing a signature from another route
+
+    if abs(time.time() - int(timestamp)) > max_age:
+        return False
+
+    payload = f"{timestamp}:{route}".encode('utf-8')
+    expected = hmac.new(ADMIN_SECRET, payload, HASH).hexdigest()
+    
+    return hmac.compare_digest(expected, signature)
+    
 
 @admin_bp.route("/restart", methods=["POST"])
 def restart_app():
-    token = request.headers.get('Admin-Token')
-    if not token or token != ADMIN_TOKEN:
+
+    timestamp = request.headers.get('Timestamp')
+    signature = request.headers.get('Signature')
+
+    if not all([timestamp, signature]):
+        print("Here 1")
         abort(403)
 
+    if not verify_signature("restart", timestamp, signature):
+        print("Here 2")
+
+        abort(403)
+    
     results = {}
 
     # 1. Extinction de celery
