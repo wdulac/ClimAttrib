1e34591482e2ce5ce0cdae329a456413ad135a2f
DULAC William <william.dulac@meteo.fr>
2026-04-14 13:53:31 +0200

    Set PR soft max to 10 000, FAR at 99.9 with 3 sig above 99

--- src/formatting/__settings.py
+++ src/formatting/__settings.py
@@ -6,10 +6,10 @@ RET_SOFT_MAX = 1 / (1e-2 * PROB_SOFT_MIN) # e.g 0.01 % -> 10 000 year return per
 
 DEFAULT_THOUSAND_SEP = ","
 
-PR_SOFT_MIN = 0.00001
-PR_SOFT_MAX = 100000
+PR_SOFT_MIN = 0.001
+PR_SOFT_MAX = 1000
 
 FAR_SIG_THRESHOLD = 99 # FAR threshold (%) above which the amount of significant digits is increased
-FAR_SIG_SECONDARY = DEFAULT_SIG + 2
-FAR_SOFT_MAX = 99.99 # Expressed as percentage
+FAR_SIG_SECONDARY = DEFAULT_SIG + 1
+FAR_SOFT_MAX = 99.9 # Expressed as percentage
 FAR_NAN_STR = '--'
